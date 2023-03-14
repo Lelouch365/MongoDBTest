29191b012e63ea74ecb47df4423bcdfd9718f9db
Lelouch365 <Luis.Partlow@Gmail.Com>
2023-03-14 14:53:10 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from creature import Creature
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("Index.html")
+
+@app.route("/den")
+def den():
+    all_creatures = Creature.objects()
+
+    return render_template("den.html",
+                           all_creatures=all_creatures)
+
+@app.route("/creature")
+def creature():
+    return render_template("creature.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
